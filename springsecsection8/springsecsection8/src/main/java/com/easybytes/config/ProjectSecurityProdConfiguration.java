3fc68d26adb08f0ca2301a4a6d8271b310d6dd0a
kamya-creator <kamyap2000@gmail.com>
2024-12-16 20:24:50 +0530

    UI app

---
+++ springsecsection8/springsecsection8/src/main/java/com/easybytes/config/ProjectSecurityProdConfiguration.java
@@ -0,0 +1,58 @@
+package com.easybytes.config;
+
+
+import com.easybytes.exceptionhandling.CustomAccessDeniedHandler;
+import com.easybytes.exceptionhandling.CustomAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("prod")
+public class ProjectSecurityProdConfiguration {
+
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        //http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        //http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // Allow only HTTPS requests at port 8443
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/contact","/notices","/error","/register").permitAll());
+
+        http.formLogin(withDefaults());
+        // httpBasic(withDefaults()) provides default no exception handling at authenticationEntryPoint and accessDenied case
+       /// http.httpBasic(withDefaults());
+
+
+
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+        // http.exceptionHandling( ehc -> ehc.authenticationEntryPoint(new CustomAuthenticationEntryPoint())); // Global Authentication Exception handler
+
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+        return http.build();
+    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+
+}
